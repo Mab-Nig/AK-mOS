c04faf8f24585735aeeca32ed312a5cb93b439b4
Mab-Nig <maivuphong4@gmail.com>
2026-02-14 18:17:08 +0700

    Reformat: tabs -> spaces.

--- AK-mOS/Src/os_cpu.c
+++ AK-mOS/Src/os_cpu.c
@@ -36,17 +36,19 @@ extern "C"
 #endif
     void os_cpu_SVCHandler(void)
     {
+        /* clang-format off */
         __asm volatile(
-            "CPSID   I				\n" // Prevent interruption during context switch
-            "LDR     R1, =tcb_curr_ptr 	\n"         // get pointer to TCB current
-            "LDR     R1, [R1]			\n" // get TCB current	= pointer to StkPtr
-            "LDR     R0, [R1]             	\n" // get StkPtr
-            "LDMIA R0!, {R4-R11}        	\n" //
-            "MSR     PSP, R0              	\n" //
-            "ORR LR, #0xD                	\n" // LR = 0xFFFFFFFD return to threadmode
-            "CPSIE   I				\n" //
-            "BX      LR				\n" //
+            "CPSID   I                   \n" // Prevent interruption during context switch
+            "LDR     R1, =tcb_curr_ptr   \n" // get pointer to TCB current
+            "LDR     R1, [R1]            \n" // get TCB current    = pointer to StkPtr
+            "LDR     R0, [R1]            \n" // get StkPtr
+            "LDMIA   R0!, {R4-R11}       \n" //
+            "MSR     PSP, R0             \n" //
+            "ORR     LR, #0xD            \n" // LR = 0xFFFFFFFD return to threadmode
+            "CPSIE   I                   \n" //
+            "BX      LR                  \n" //
         );
+        /* clang-format on */
     }
 #ifdef __cplusplus
 }
@@ -60,34 +62,36 @@ extern "C"
 #if 1
     void os_cpu_PendSVHandler(void)
     {
+        /* clang-format off */
         __asm volatile(
-            //"CPSID   I                       		\n"	//Prevent interruption during context switch
-            "MRS     R0, PSP                     		\n" // PSP is process stack pointer
-            "CBZ     R0, OS_CPU_PendSVHandler_nosave   	\n" // Skip register save the first time
+            //"CPSID   I                                   \n" // Prevent interruption during context switch
+            "MRS     R0, PSP                             \n" // PSP is process stack pointer
+            "CBZ     R0, OS_CPU_PendSVHandler_nosave     \n" // Skip register save the first time
 
-            "SUBS    R0, R0, #0x20               		\n" // Save remaining regs r4-11 on process stack
-            "STM     R0, {R4-R11}				\n" //
+            "SUBS    R0, R0, #0x20                       \n" // Save remaining regs r4-11 on process stack
+            "STM     R0, {R4-R11}                        \n" //
 
-            "LDR     R1, =tcb_curr_ptr               	\n" // OSTCBCur->OSTCBStkPtr = SP;
-            "LDR     R1, [R1]\n"                            //
-            "STR     R0, [R1]                    		\n" // R0 is SP of process being switched out
+            "LDR     R1, =tcb_curr_ptr                   \n" // OSTCBCur->OSTCBStkPtr = SP;
+            "LDR     R1, [R1]                            \n" //
+            "STR     R0, [R1]                            \n" // R0 is SP of process being switched out
 
-            /* At this point, entire context of process has been saved	*/
+            /* At this point, entire context of process has been saved    */
 
-            "OS_CPU_PendSVHandler_nosave:			\n" //
-            "LDR     R0, =tcb_curr_ptr               	\n" // OSTCBCur  = OSTCBHighRdy;
-            "LDR     R1, =tcb_high_rdy_ptr			\n" //
-            "LDR     R2, [R1]					\n" //
-            "STR     R2, [R0]					\n" //
+            "OS_CPU_PendSVHandler_nosave:                \n" //
+            "LDR     R0, =tcb_curr_ptr                   \n" // OSTCBCur  = OSTCBHighRdy;
+            "LDR     R1, =tcb_high_rdy_ptr               \n" //
+            "LDR     R2, [R1]                            \n" //
+            "STR     R2, [R0]                            \n" //
 
-            "LDR     R0, [R2]                    		\n" // R0 is new process SP; SP = OSTCBHighRdy->OSTCBStkPtr;
-            "LDM     R0, {R4-R11}                		\n" // Restore r4-11 from new process stack
-            "ADDS    R0, R0, #0x20				\n" //
-            "MSR     PSP, R0                     		\n" // Load PSP with new process SP
-            "ORR     LR, LR, #0x04               		\n" // Ensure exception return uses process stack
-            //"CPSIE   I						\n"  	//
-            "BX      LR                          		\n" // Exception return will restore remaining context
+            "LDR     R0, [R2]                            \n" // R0 is new process SP; SP = OSTCBHighRdy->OSTCBStkPtr;
+            "LDM     R0, {R4-R11}                        \n" // Restore r4-11 from new process stack
+            "ADDS    R0, R0, #0x20                       \n" //
+            "MSR     PSP, R0                             \n" // Load PSP with new process SP
+            "ORR     LR, LR, #0x04                       \n" // Ensure exception return uses process stack
+            //"CPSIE   I                                   \n" //
+            "BX      LR                                  \n" // Exception return will restore remaining context
         );
+        /* clang-format on */
     }
 #endif
 
